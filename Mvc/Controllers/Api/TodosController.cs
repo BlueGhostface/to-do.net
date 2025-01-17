7f0178b9d97fb4b2a709a5a0656e10eeb3cdcb1c
axell.tielemans <axelltielemans@gmail.com>
2025-01-17 23:35:49 +0100

    update to api todocontroller

--- Mvc/Controllers/Api/TodosController.cs
+++ Mvc/Controllers/Api/TodosController.cs
@@ -1,6 +1,37 @@
-﻿namespace Mvc.Controllers.Api;
+﻿using BL.Todos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class TodosController
+namespace Mvc.Controllers.Api;
+
+[ApiController]
+[Route("api/Todos")]
+public class TodosController : Controller
 {
+    
+    private readonly ITodoManager _todoManager;
+
+    public TodosController(ITodoManager todoManager)
+    {
+        _todoManager = todoManager;
+    }
+    
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult GetTodos()
+    {
+        var todos = _todoManager.GetAllTodos();
+        return Ok(todos);
+    }
+    
+    [HttpGet]
+    [AllowAnonymous]
+    [Route("{id:int}")]
+    public IActionResult GetTodoById(int id)
+    {
+        var todo = _todoManager.GetTodoById(id);
+        return Ok(todo);
+    }
+    
     
 }
