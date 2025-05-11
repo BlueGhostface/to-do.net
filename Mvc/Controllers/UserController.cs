7f53188a962e5a074f0d154cb5b982766c67fbc0
axell.tielemans <axelltielemans@gmail.com>
2025-05-11 22:25:32 +0200

    started user profiles

---
+++ Mvc/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.Models.User;
+
+namespace Mvc.Controllers;
+
+public class UserController : Controller
+{
+    
+    [HttpGet]
+    public IActionResult UserProfile()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult UserProfile(string name, string email)
+    {
+        var user = new UserViewModel()
+        {
+            name = name,
+            Email = email
+        };
+        return View(user);
+    }
+    
+}
